088343db881bb5bbe65fc32b6971ca892c0ae798
haven <haven@haven.(none)>
2018-09-27 17:01:22 -0400

    date snapshot

--- gittyup.sh
+++ gittyup.sh
@@ -4,8 +4,18 @@
 
 # cron job script to commit whatever code changes have
 # been made to haven from the mud/ftp whatever
+
+# change to mud dir
 cd /home/haven/haven
+
+# make a variable for current date (human readable)
 sDATE=date
 
+# add eerthing
 git add .
+
+# commit changes
 git commit -m "$sDATE snapshot"
+
+# push to github
+git push origin master
