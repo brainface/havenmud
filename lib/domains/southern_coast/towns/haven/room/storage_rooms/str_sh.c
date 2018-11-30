114e36a03c39a7c77df1720df49d158431983f4c
haven <haven@haven.(none)>
2018-11-30 09:00:01 -0500

    Fri Nov 30 09:00:01 EST 2018 snapshot

--- lib/domains/southern_coast/towns/haven/room/storage_rooms/str_sh.c
+++ lib/domains/southern_coast/towns/haven/room/storage_rooms/str_sh.c
@@ -14,16 +14,15 @@ inherit LIB_STOREROOM;
 static void create() {
    storeroom::create();
    SetInventory( ([
-     
+
      STD_LIGHT "torch" : 8,
      STD_FISH "pole" : 2,
      H_OBJ + "pole" : 2,
-//     STD_STORAGE "backpack" : 2
+     STD_STORAGE "backpack" : 2,
      STD_OBJ "parchment" : 3,
      STD_OBJ "digging/shovel" : 1,
+     STD_OBJ "digging/hoe" : 1,
    ]) );
    SetPurgeInterval(1);
 }
 
-
-/* Approved by Haun on Tue Apr 27 21:19:08 1999. */
