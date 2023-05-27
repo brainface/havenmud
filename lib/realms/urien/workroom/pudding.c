c1ee76a4d00bae335dae534f01197a1999a38ce8
sabosan <sleighedem@gmail.com>
2023-05-27 03:00:01 -0400

    Sat May 27 03:00:01 AM EDT 2023 snapshot

--- lib/realms/urien/workroom/pudding.c
+++ lib/realms/urien/workroom/pudding.c
@@ -1,6 +1,9 @@
 // Banana Pudding with Beaver butt flavoring
-// It's true. Look it up.
-// www.nationalgeographic.com/animals/article/beaver-butt-goo-vanilla-flavoring
+// Look it up. Castoreum.
+// www.youtube.com/watch?v=nweK6VRM8a8&ab 
+// www.youtube.com/watch?v=ZKrj3UjMi1o&ab
+// www.youtube.com/watch?v=rW_7AWhQfK8&ab (LTN/CNN seriousish vid)
+// Was a radio show segment that I found quite funny / assess intake
 // Also I dont think I ever made food with a 'wrapper' 
 // Urien 5/26/2023
 
@@ -20,8 +23,9 @@ static void create() {
           "in a sad attempt to schmooze, make amends or "
           "festivities it was created in haste for you! The "
           "ingredients seem to have been all blended together "
-          "into a light fluffy creme ooze inside a convenient "
-          "clear plastic container.");
+          "into a light fluffy creamy ooze inside a convenient "
+          "clear plastic container. It almost appears other "
+          "worldly in packaging and convenience.");
   SetMealMessages( ({
     "$N eats the entire portion of banana pudding.",
     "You eat the banana pudding. It tastes delicious with a hint "
@@ -33,8 +37,9 @@ static void create() {
   SetValue(10);
   SetEmptyName("container");
   SetEmptyShort("an empty container of banana pudding");
-  SetEmptyLong("This empty container once held the remnants "
+  SetEmptyLong("This plastic container once held the remnants "
     "of some banana pudding. Some transparent slime of what "
     "once along the inside would only provide flavoring, "
-    "nothing more for sustenance.");
+    "nothing more for sustenance. A sad state for what once "
+    "was.");
 }
